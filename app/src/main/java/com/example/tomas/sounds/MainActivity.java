481c2fc7c74a29898018caa08ae56cb14f25a213
tomas <tomaszskorupa@hotmail.com>
2019-07-21 04:03:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tomas/sounds/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.tomas.sounds;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void PlayWakeTheFuckUp(View view) {
+        stop();
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.wake_the_fuck_up_samurai);
+        mediaPlayer.start();
+        }
+
+    public void PlayKurwa(View view) {
+        stop();
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.o_kurwa);
+        mediaPlayer.start();
+    }
+
+    public void PlayWypierdalaj(View view) {
+        stop();
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.wypierdalac);
+        mediaPlayer.start();
+    }
+
+    private void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        }
+}
+
