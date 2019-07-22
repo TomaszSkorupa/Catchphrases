a29b6f098edc9bb5caa7f935c27e5f0fac034247
tomas <tomaszskorupa@hotmail.com>
2019-07-22 02:28:55 +0200

    I've made the status bar translucent

--- app/src/main/java/com/example/tomas/sounds/MainActivity.java
+++ app/src/main/java/com/example/tomas/sounds/MainActivity.java
@@ -4,16 +4,20 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    Window window;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        setStatusBarTranslucent();
     }
 
     public void PlayWakeTheFuckUp(View view) {
@@ -34,12 +38,18 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.start();
     }
 
-    private void stop() {
+    public void stop() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             mediaPlayer.release();
             mediaPlayer = null;
         }
     }
+
+    protected void setStatusBarTranslucent() {
+        window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+
 }
 
